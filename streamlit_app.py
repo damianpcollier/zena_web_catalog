590ed3f5323a9bb40cb860db70d50ac9bad47478
Damian Collier <damian.collier@hotmail.com>
2022-11-25 11:13:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
-##df = pandas.DataFrame(my_catalog)
+df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
 # put the first column into a list
-##color_list = df[0].values.tolist()
+color_list = df[0].values.tolist()
 # print(color_list)
 # Let's put a pick list here so they can pick the color
 ##option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
